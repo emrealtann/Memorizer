78c23c263962e941bbe1a65afd953db4b88cf138
emrealtann <emre.altan2@gmail.com>
2017-02-20 16:28:52 +0200

    Create new mvc solution.

---
+++ Memorizer/Memorizer/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Memorizer.Controllers
+{
+    public class HomeController : Controller
+    {
+        Dictionary<string, string> items = new Dictionary<string, string>();
+        public ActionResult Index()
+        {
+            GetItem("");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public JsonResult GetItem(string id)
+        {
+            ReadAllItems("~/Resources/words.txt");
+            if (String.IsNullOrEmpty(id))
+            {
+                Random rnd = new Random();
+                return Json(items.ElementAt(rnd.Next(0, items.Count)));
+            }
+            return Json(items.First(x => x.Key.Equals(id)));
+        }
+
+        public void ReadAllItems(string path)
+        {
+            string line = "";
+            var items = new Dictionary<string, string>();
+
+            var filestream = new System.IO.FileStream(Server.MapPath(path),
+                                          System.IO.FileMode.Open,
+                                          System.IO.FileAccess.Read,
+                                          System.IO.FileShare.ReadWrite);
+            var file = new System.IO.StreamReader(filestream, System.Text.Encoding.UTF8, true, 128);
+
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] lineArray = line.Split(';');
+                items.Add(lineArray[0], lineArray[1]);
+            }
+
+            this.items = new Dictionary<string, string>(items);
+        }
+    }
+}
---
+++ Memorizer/Memorizer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Memorizer.Startup))]
+namespace Memorizer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
